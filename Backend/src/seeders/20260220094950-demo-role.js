5627cc84b973040ae6140d84d44e091ab10fa794
Rekia Mansour Ouahchia <rmansourouahchia@gmail.com>
2026-02-20 11:09:19 +0100

    creat role seed

---
+++ Backend/src/seeders/20260220094950-demo-role.js
@@ -0,0 +1,35 @@
+'use strict';
+const { v4: uuidv4 } = require("uuid");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Role', [
+      {
+        id: uuidv4(),
+        name: 'Admin',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Teacher',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Student',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+    ], {});
+
+  },
+
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('Role', null, {});
+  }
+};
