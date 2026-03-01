a699c927693b66cb2a8cbd0c307edbfa9be79236
Rekia Mansour Ouahchia <rmansourouahchia@gmail.com>
2026-03-01 15:22:34 +0100

    add user api (routes + contrillers)

--- Backend/src/routes/user.route.ts
+++ Backend/src/routes/user.route.ts
@@ -1,10 +1,24 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller";
+import { createUser, getUsers, deleteUser, updateUserRole, updateUser, getUserProfile } from "../controllers/user.controller";
 import { validate } from "../middlewares/validate";
 import { createUserSchema } from "../models/User.Model";
+import { authenticateToken } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/role.middelware";
+
 
 const router = express.Router();
+//register
+router.post("/register", validate(createUserSchema), createUser);
+//get all users (admin only)
+router.get("/", authenticateToken, authorize(["Admin"]), getUsers);
+//delete user by id (admin only)
+router.delete("/:id", authenticateToken ,authorize(["Admin"]) , deleteUser);
+//update user role by id (admin only)
+router.put("/:id/role", authenticateToken,authorize(["Admin"]), updateUserRole);
 
-router.post("/register",validate(createUserSchema),createUser);
+//update user (by user)
+router.put("/me", authenticateToken, updateUser);
+//get user profile
+router.get("/me", authenticateToken, getUserProfile);
 
 export default router;
